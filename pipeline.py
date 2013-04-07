1e12c63dcf606c224dcedab6d00b77112dc79039
Igor Rios <igorlrios@gmail.com>
2013-04-07 17:30:40 -0300

    variaveis do pipeline prontas

--- pipeline.py
+++ pipeline.py
@@ -23,12 +23,13 @@ class Pipeline(object):
 	PC_counter = 0
 	
 	controlSignals = {}
-	controlSignals["RegDst"]=0
-	controlSignals["ALUSrc"]=0
-	controlSignals["MemtoReg"]=0
-	controlSignals["RegDst"]=0
-	controlSignals["RegDst"]=0
-	controlSignals["RegDst"]=0
-	controlSignals["RegDst"]=0
+	controlSignals["RegDst"] = 0
+	controlSignals["ALUSrc"] = 0
+	controlSignals["MemtoReg"] = 0
+	controlSignals["RegWrite"] = 0
+	controlSignals["MemWrite"] = 0
+	controlSignals["Branch"] = 0
+	controlSignals["Jump"] = 0
+	controlSignals["ExtOp"] = 0
+	controlSignals["ALUctr<2:0>"] = None
 	
-	#def __init__ (self, )
