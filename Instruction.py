81a379b018bbbacaefb7060e438534804a5a69d1
root <root@ubuntu.(none)>
2013-04-07 16:27:53 -0300

    Added Instruction abstract class

---
+++ Instruction.py
@@ -0,0 +1,17 @@
+class Instruction():
+    code = ""
+
+    def action_IF():
+        pass
+        
+    def action_ID():
+        pass
+
+    def action_EX():
+        pass
+
+    def action_MEM():
+        pass        
+
+    def action_WB():
+        pass                
