7ffe7d0dbc8e11ad0c01ff1cd2073406c007cd52
Igor Rios <igorlrios@gmail.com>
2013-04-07 16:23:55 -0300

    Arquivos

---
+++ program_controller.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+class ProgramController(object):
+	def run_clock(self):
+		pass
+	
+	def run_entire_program(self):
+		pass
+	
+	def recently_used_mem(self):
+		pass
+	
+	def clock_number(self):
+		pass
