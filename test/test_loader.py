b4267095e5d30618ef91c337085b9c087dcdcedf
Marcelo Almeida <marcelocralmeida@gmail.com>
2013-04-07 18:47:30 -0300

    Loader pronto

---
+++ test/test_loader.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import unittest
+import sys
+sys.path.append("../")
+from loader import Loader
+
+
+class TestLoader(unittest.TestCase):
+    def setUp(self):
+        self.loader = Loader()
+
+# TODO: implementar os testes
