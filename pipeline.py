58d22bbe84834844a81eb09bb0ffd06dabe01e22
Igor Rios <igorlrios@gmail.com>
2013-04-07 17:11:50 -0300

    arquivos pipeline_phase_XX e pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,34 @@
 # coding: utf-8
 
 class Pipeline(object):
-	pass
+	
+	
+	registers = []
+	for i in range(32):
+		registers.append(Register())
+	
+	phase = []
+	phase.append(PipelinePhaseIF())
+	phase.append(PipelinePhaseID())
+	phase.append(PipelinePhaseEX())
+	phase.append(PipelinePhaseMEM())
+	phase.append(PipelinePhaseWB())
+	
+	memX = []
+	memY = []
+	
+	dados = []
+	
+	PC = []
+	PC_counter = 0
+	
+	controlSignals = {}
+	controlSignals["RegDst"]=0
+	controlSignals["ALUSrc"]=0
+	controlSignals["MemtoReg"]=0
+	controlSignals["RegDst"]=0
+	controlSignals["RegDst"]=0
+	controlSignals["RegDst"]=0
+	controlSignals["RegDst"]=0
+	
+	#def __init__ (self, )
---
+++ pipeline_phase_ID.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+class PipelinePhaseID():
+	current_instruction = None
+	def action();
+		pass
+	
